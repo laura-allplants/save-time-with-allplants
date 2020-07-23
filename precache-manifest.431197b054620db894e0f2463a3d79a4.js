176e67bfc245447f0ad9125abec9cdf12326ba33
Laura <laura@Lauras-MacBook-Air-2.local>
2020-07-23 10:30:57 +0100

    Updates

---
+++ precache-manifest.431197b054620db894e0f2463a3d79a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69073be1dc637bb6e35813acb82dbed5",
+    "url": "/save-time-with-allplants/index.html"
+  },
+  {
+    "revision": "85370d83cdcee1ce0c66",
+    "url": "/save-time-with-allplants/static/css/main.3939d98b.chunk.css"
+  },
+  {
+    "revision": "b6f2482942a5b659e47d",
+    "url": "/save-time-with-allplants/static/js/2.8b7e9912.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/save-time-with-allplants/static/js/2.8b7e9912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85370d83cdcee1ce0c66",
+    "url": "/save-time-with-allplants/static/js/main.62b07d34.chunk.js"
+  },
+  {
+    "revision": "4e365030dc6b13295bce",
+    "url": "/save-time-with-allplants/static/js/runtime-main.0f40f79f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/save-time-with-allplants/static/media/logo.5d5d9eef.svg"
+  }
+]);
